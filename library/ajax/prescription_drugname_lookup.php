f6a86286f2891b13e47113564f535c013108ca0c
bradymiller <bradymiller@users.sourceforge.net>
2012-11-11 03:14:12 -0800

    Integration of rxnorm into prescriptions.
     -Imported a full jquery-ui codeset.
     -Implemented autocomplete to search the drugs and rxnorm
    
     TODO (a lot):
     -rxnorm looking good now. just some glitches when choosing combo drugs by brand names.
     -process the selected entry into the prescription form
     -ensure placing correct rxnorm id(s) into the entry in prescriptions sql tables
     -once autocomplete looks good, implement it into the code set searches
     -continue playing around with limit, which should be able to make very high when
      get css formatting and images worked out
     -i don't think the in-progress graphic is working. need to test when have longer
      run time queries happening

--- library/ajax/prescription_drugname_lookup.php
+++ library/ajax/prescription_drugname_lookup.php
@@ -1,34 +1,82 @@
 <?php
-// Copyright (C) 2008 Jason Morrill <jason@italktech.net>
-//
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public License
-// as published by the Free Software Foundation; either version 2
-// of the License, or (at your option) any later version.
-// 
-// This file use is used specifically to look up drug names when
-// writing a prescription. See the file:
-//    templates/prescriptions/general_edit.html
-// for additional information
-//
-// Important - Ensure that display_errors=Off in php.ini settings.
-//
+/**
+ * This file use is used specifically to look up drug names when
+ * writing a prescription.
+ *
+ * See the file templates/prescriptions/general_edit.html
+ * for additional information.
+ *
+ * Copyright (C) 2008 Jason Morrill <jason@italktech.net>
+ *
+ * LICENSE: This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://opensource.org/licenses/gpl-license.php>;.
+ *
+ * @package OpenEMR
+ * @author  Jason Morrill <jason@italktech.net>
+ * @author  Brady Miller <brady@sparmy.com>
+ * @link    http://www.open-emr.org
+ */
+
+$fake_register_globals=false;
+$sanitize_all_escapes=true;
+
 include_once("../../interface/globals.php");
-include_once("{$GLOBALS['srcdir']}/sql.inc");
+include_once("{$GLOBALS['srcdir']}/rxnorm.inc.php");
 include_once("{$GLOBALS['srcdir']}/formdata.inc.php");
 
-$q = formData("q","G",true);
-if (!$q) return;
-$limit = $_GET['limit'];
+// This is only pertinent for users of php versions less than 5.2
+//  (ie. this wrapper is only loaded when php version is less than
+//   5.2; otherwise the native php json functions are used)
+require_once "$srcdir/jsonwrapper/jsonwrapper.php";
 
-$sql = "select drug_id, name from drugs where ".
-            " name like ('".$q."%')".
-            " order by name ".
-            " limit ".$limit;
-$rez = sqlStatement($sql);
+// Collect the search term
+$term = trim($_GET['term']);
+if (empty($term)) return;
 
-while ($row = sqlFetchArray($rez)) {
-    echo $row['name']."|".$row['drug_id']."\n";
+// Collect the limit, if applicable
+$limit = trim($_GET['limit']);
+
+// Search
+//  If rxnnorm tables exist, then search those.
+//  If rxnorm tables do not exist, then search the drugs table.
+//  @TODO In future, will need to likely integrate the rxnorm search with drugs table
+//    with the drugs table search.
+$rxnorm_flag = false;
+$check_table = sqlQuery("SHOW TABLES LIKE 'RXNCONSO'");
+if ( !(empty($check_table)) ) {
+  $rxnorm_flag = true;
 }
+if ($rxnorm_flag) {
+  //Search Rxnorm Database
+  $arr = search_drug_names_rxnorm($term,$limit);
+}
+else {
+  //Search drugs table
+  if (!empty($limit)) {
+    $limit = " limit " . add_escape_custom($limit);
+  }
+  else {
+    $limit = "";
+  }
+  $sql = "select drug_id, name, 'drugs' as drug_database from drugs where ".
+            " name like ?".
+            " order by name $limit";
+  $rez = sqlStatement($sql, array($term."%") );
+  $arr = array();
+  for($iter=0; $row=sqlFetchArray($rez); $iter++) {
+    $arr[$iter]=$row;
+  }
+}
+
+// Return the results
+echo json_encode($arr);
 
 ?>
