f4a347ff3fcae436bb865f6696316dd189936fed
bradymiller <bradymiller@users.sourceforge.net>
2013-04-28 10:48:02 -0700

    Version 2 Scanned Documents, INTERIM CODE.
     -Still lots to do:
       -Add ability to modify/change document.
       -Add ability to completely delete it.
       -Deal with in the custom reports (the documents works in Rod's
        pdf, but breaks with scanned encounter form; also need to
        figure out if only give option to see them in scanned forms)
     -Deal with the report and encounter view (will need to
      possible break it into piece images etc)
     -Keep testing.

--- sql/4_1_1-to-4_1_2_upgrade.sql
+++ sql/4_1_1-to-4_1_2_upgrade.sql
@@ -410,3 +410,9 @@ ALTER TABLE `issue_types` ADD COLUMN `active` tinyint(1) NOT NULL DEFAULT '1';
 ALTER TABLE `documents` ADD COLUMN `path_depth` TINYINT DEFAULT '1' COMMENT 'Depth of path to use in url to find document. Not applicable for CouchDB.';
 #Endif
 
+#IfNotRow categories name Scanned Encounter Notes
+INSERT INTO categories select (select MAX(id) from categories) + 1, 'Scanned Encounter Notes', '', 1, rght, rght + 1 from categories where name = 'Categories';
+UPDATE categories SET rght = rght + 2 WHERE name = 'Categories';
+UPDATE categories_seq SET id = (select MAX(id) from categories);
+#Endif
+
