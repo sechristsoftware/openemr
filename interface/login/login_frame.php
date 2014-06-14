df94c6c47e5c92a4e11868d28616ca4f3c221607
Robert Down <robertdown@live.com>
2014-06-14 12:07:50 -0400

    PSR2 Compliance

--- interface/login/login_frame.php
+++ interface/login/login_frame.php
@@ -2,25 +2,52 @@
 $ignoreAuth = true;
 include_once ("../globals.php");
 ?>
-<HTML>
+<html>
 <head>
-<?php html_header_show(); ?>
-<TITLE><?php xl ('Login','e'); ?></TITLE>
-<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-<link rel=stylesheet href="<?php echo $css_header;?>" type="text/css">
-<link rel=stylesheet href="../themes/login.css" type="text/css">
+    <?php html_header_show(); ?>
+    <title><?php xl('Login', 'e'); ?></title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <link rel=stylesheet href="<?php echo $css_header;?>" type="text/css">
+    <link rel=stylesheet href="../themes/login.css" type="text/css">
+</head>
 
-</HEAD>
-
-<frameset rows="<?php echo "$GLOBALS[logoBarHeight],$GLOBALS[titleBarHeight]" ?>,*" cols="*" frameborder="NO" border="0" framespacing="0">
-  <frame class="logobar" src="<?php echo $rootdir;?>/login/filler.php" name="Filler Top" scrolling="no" noresize frameborder="NO">
-  <frame class="titlebar" src="<?php echo $rootdir;?>/login/login_title.php" name="Title" scrolling="no" noresize frameborder="NO">
-  <frame src="<?php echo $rootdir;?>/login/login.php" name="Login" scrolling="auto" frameborder="NO">
-  <!--<frame src="<?php echo $rootdir;?>/login/filler.php" name="Filler Bottom" scrolling="no" noresize frameborder="NO">-->
+<frameset
+    rows="<?php echo "$GLOBALS[logoBarHeight], $GLOBALS[titleBarHeight]" ?>,*"
+    cols="*"
+    frameborder="no"
+    border="0"
+    framespacing="0">
+    <frame
+        class="logobar"
+        src="<?php echo $rootdir;?>/login/filler.php"
+        name="Filler Top"
+        scrolling="no"
+        noresize
+        frameborder="no">
+    <frame
+        class="titlebar"
+        src="<?php echo $rootdir;?>/login/login_title.php"
+        name="Title"
+        scrolling="no"
+        noresize
+        frameborder="no">
+    <frame
+        src="<?php echo $rootdir;?>/login/login.php"
+        name="Login"
+        scrolling="auto"
+        frameborder="no">
+    <!--
+    <frame
+        src="<?php echo $rootdir;?>/login/filler.php"
+        name="Filler Bottom"
+        scrolling="no"
+        noresize
+        frameborder="no">
+    -->
 </frameset>
+<noframes>
+    <body bgcolor="#ffffff">
 
-<noframes><body bgcolor="#FFFFFF">
-
-</body></noframes>
-
-</HTML>
+    </body>
+</noframes>
+</html>
